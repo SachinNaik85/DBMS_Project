fb2ce1db2213ab19dd06e96e8301a0d5f2598a7f
SachinNaik85 <naiksachin8585@gmail.com>
2020-05-14 11:05:47 +0530

    video call changes on 14-05-2020

--- search/bus_and_hotel.py
+++ search/bus_and_hotel.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from essential import credential
-db = mysql.connector.connect(host="localhost", user="root", passwd=credential, database="travel")
+db = mysql.connector.connect(host="localhost", user="root", passwd=credential['password'], database="travel")
 sql = db.cursor()
 
 
@@ -27,6 +27,27 @@ def create_bus():
 
     return buses
 
+
+def create_hotel():
+    sql.execute("select * from bus;")
+    hotel_data = sql.fetchall()
+    hotels = []
+
+    class Hotel:
+        hotel_name = str
+        hotel_type = str
+        hotel_price = str
+        hotel_location = str
+
+    for i in hotel_data:
+        hotel1 = Hotel()
+        hotel1.hotel_name = i[0]
+        hotel1.hotel_type = i[1]
+        hotel1.hotel_price = i[2]
+        hotel1.hotel_location = i[3]
+        hotels.append(hotel1)
+
+    return hotels
 
 
 
