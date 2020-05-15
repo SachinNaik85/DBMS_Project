1cbb3d5398cd3c911ffdce3b8f28722b929c0c0c
URYashaswini <yashu1999.m@gmail.com>
2020-05-15 19:20:05 +0530

    by yashu

--- static/js/login.js
+++ static/js/login.js
@@ -1,19 +1,16 @@
-function validate()
-{
+function validate() {
     var username_regx = /^[a-zA-Z0-9_$.@]+$/
     var password_regx = /^(?=.*\d).{4,12}$/
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var allclear = true;
-    if (! (username_regx.test(username)))
-    {
+    if (!(username_regx.test(username))) {
         allclear = false;
         document.getElementById('warning').style = "visibility : visible";
         document.getElementById('warning').innerHTML = "invalid username";
     }
 
-    if (! (password_regx.test(password)))
-    {
+    if (!(password_regx.test(password))) {
         allclear = false;
         document.getElementById('warning').style = "visibility : visible";
         document.getElementById('warning').innerHTML = "invalid password";
@@ -21,3 +18,9 @@ function validate()
     return allclear;
 }
 
+function close_reset() {
+    var modalpopup = document.getElementsByClassName('reset_password');
+    for (var i = 0; i < modalpopup.length; i++) {
+        modalpopup[i].style = "visibility: hidden ; filter : blur(0px)";
+    }
+}
