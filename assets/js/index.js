5688dd0fa4c615118063fa027d31a418512cfc87
SachinNaik85 <naiksachin8585@gmail.com>
2020-05-16 16:55:26 +0530

    added urls for hotel and bus

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
-function hide()
+function details()
 {
-    console.log('user logged in');
+    var user_information = document.getElementsByClassName('user_information');
+    for (var i=0; i<user_information.length; i++)
+    {
+        user_information[i].style = "visibility : visible";
+    }
 }
+
